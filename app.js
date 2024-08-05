5490e4ea31340fb232f698355b3e35166a6a17ca
Jeffrey Li <128416729+Jdf117@users.noreply.github.com>
2024-08-04 17:37:37 -0700

    CRUD operations complete. Added Note router

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = 3000;
 
-//include Schema 
-const schema = require("./models/noteSchema");
-const Note = schema.Note;
+const notesRouter = require("./routes/notesRouter");
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -36,72 +34,6 @@ app.get("/", (req, res) => {
     res.render("index");
 })
 
-//Gets the full list of notes
-app.get("/notes", async (req, res) => {
-    console.log("finding notes");
-    const  notes = await Note.find();
-
-    if(notes.length > 0){
-        console.log("Notes exist");
-        res.status(200).json(notes);
-    } else {
-        console.log("empty database");
-        res.status(200).send("Database is empty");
-    }
-})
-
-
-//Creates a new note. If no title is specified, it will default to today's date
-app.post("/note", async (req, res) => {
-    const title = req.body.title; 
-    const content = req.body.content;
-    const  notes = await Note.find();
-    const id = notes.length + 1;
-    console.log("Note ID: " +  id);
-    try{
-        const newNote = new Note({id, title, content});
-        console.log(newNote);
-        await newNote.save();
-        res.status(201).send("Note added successfully");
-    } 
-    catch (err) {
-        console.log("Could not add Note");
-    }
-});
-
-//Delete note via ID 
-app.delete('/notes/:id', async (req, res) => {
-    const id = req.params.id;
-    try{
-        // need to check if note exists 
-        const note = await Note.findById(id);
-        if(note){
-            await Note.deleteOne({_id: new mongoose.Types.ObjectId(id)});
-            res.status(200).send("Note deleted successfully");
-        } else {
-            res.status(404).send("Note not found");
-        }
-
-    } catch(err){
-        res.send("Could not delete the note");
-    }
-})
-//delete all
-app.delete('/delete-notes', async (req,res) => {
-    try{
-        if(!Note.length() == 0 ){
-            await Note.deleteMany({});
-            res.status(200).send("Deleted all notes");
-        } 
-
-    } catch (err){
-        res.send("Could not delete notes");
-    }
-});
-
-//Update Note via ID 
-app.put("/update-note/:id", (req, res) => {
-    const id = req.params.id;
-})
+app.use("/", notesRouter);
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
---
+++ routes/notesRouter.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const router = express.Router(); 
+const mongoose = require("mongoose");
+
+//include schema
+const schema = require("../models/noteSchema");
+const Note = schema.Note;
+
+//Gets the full list of notes
+router.get("/notes", async (req, res) => {
+    console.log("finding notes");
+    const  notes = await Note.find();
+
+    if(notes.length > 0){
+        console.log("Notes exist");
+        res.status(200).json(notes);
+    } else {
+        console.log("empty database");
+        res.status(200).send("Database is empty");
+    }
+})
+
+
+//Creates a new note. If no title is specified, it will default to today's date
+router.post("/note", async (req, res) => {
+    const title = req.body.title; 
+    const content = req.body.content;
+    const  notes = await Note.find();
+    const id = notes.length + 1;
+    console.log("Note ID: " +  id);
+    try{
+        const newNote = new Note({id, title, content});
+        console.log(newNote);
+        await newNote.save();
+        res.status(201).send("Note added successfully");
+    } 
+    catch (err) {
+        console.log("Could not add Note");
+    }
+});
+
+//Delete note via ID 
+router.delete('/notes/:id', async (req, res) => {
+    const id = req.params.id;
+    try{
+        // need to check if note exists 
+        const note = await Note.findById(id);
+        if(note){
+            await Note.deleteOne({_id: new mongoose.Types.ObjectId(id)});
+            res.status(200).send("Note deleted successfully");
+        } 
+        //else {
+            //res.status(404).send("Note not found");
+        //}
+
+    } catch(err){
+        res.send("Could not delete the note");
+    }
+})
+//delete all
+router.delete('/delete-notes', async (req,res) => {
+    try{
+        if(!Note.length() == 0 ){
+            await Note.deleteMany({});
+            res.status(200).send("Deleted all notes");
+        } 
+
+    } catch (err){
+        res.send("Could not delete notes");
+    }
+});
+
+//Update Note via ID 
+router.put("/update-note/:id", async (req, res) => {
+    const noteId = parseInt(req.params.id);
+    //const notes = await Note.find();
+ 
+    try{
+        const updatedNote = await Note.findOneAndUpdate(
+            {id: noteId},
+            {
+                title: req.body.title, 
+                content:req.body.content
+            },
+            {new: true}//option used to return the modified document 
+        );
+
+        if(updatedNote){
+            res.status(200).send("Note updated successfully");
+            console.log(updatedNote);
+        } else {
+            res.status(404).send("Note not found");
+        }
+        //Below code does not work because it only updates the local notes array and not saving these changes to the database
+        // const noteIndex = notes.findIndex((Note) => Note.id == noteId );
+        // console.log("noteIndex = " + noteIndex);
+
+        // if(noteIndex != -1) {
+        //     notes[noteIndex].title = req.body.title;
+        //     notes[noteIndex].content = req.body.content;
+        //     res.status(200).send("Note updated successfully");
+        //     console.log(notes);
+        // } 
+    } catch(err){
+        console.error(err);
+        res.status(500).send("Could not update the note");
+    }
+
+})
+
+module.exports = router;
